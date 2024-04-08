d8b2823487f0d7f3cdccb43f19829795b207aed5
parkjineon <truewords0113@naver.com>
2024-04-08 16:20:19 +0900

    feat: create memberservice

--- src/day04/MemberService.java
+++ src/day04/MemberService.java
@@ -21,7 +21,7 @@ public class MemberService{
 
         HashMap<String,User> userDB = new HashMap<>();
         User user = null;
-        DB db = new DB(userDB);
+        DAO dao = new DAO(userDB);
         int menu = 100;
         while(menu != 0){
             Scanner sc = new Scanner(System.in);
@@ -45,7 +45,7 @@ public class MemberService{
                         if (user != null) {
                             System.out.println("이미 로그인 되었습니다.");
                         } else {
-                            db.signUp();
+                            dao.signUp();
                         }
 
                         break;
@@ -53,21 +53,21 @@ public class MemberService{
                         if (user != null) {
                             System.out.println("이미 로그인 되었습니다.");
                         } else {
-                            user = db.login();
+                            user = dao.login();
                         }
 
                         break;
                     case 3:
                         if(user != null){
                             System.out.println("=== 회원 정보 수정 ===");
-                            user = db.updateInfo(user.getId());
+                            user = dao.updateInfo(user.getId());
                         }else{
                             System.out.println("로그인 상태가 아닙니다.");
                         }
                         break;
                     case 4:
                         if (user != null) {
-                            db.deleteUser(user.getId());
+                            dao.deleteUser(user.getId());
                             user = null;
                         } else {
                             System.out.println("로그인 상태가 아닙니다.");
@@ -85,7 +85,7 @@ public class MemberService{
                     case 6:
                         if (user != null) {
                             System.out.println("==== 현재 회원 정보 =====");
-                            db.getInfo(user.getId());
+                            dao.getInfo(user.getId());
                         } else {
                             System.out.println("로그인 상태가 아닙니다.");
                         }
@@ -111,116 +111,5 @@ public class MemberService{
     }
 }
 
-class User{
-    private String id;
-    private String pw;
-    private String name;
-
-    User(String id, String pw, String name){
-        this.id = id;
-        this.pw = pw;
-        this.name = name;
-    }
-
-    public void setUser(String name, String pw) {
-        this.name = name;
-        this.pw = pw;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getPw() {
-        return pw;
-    }
-
-    public String getName() {
-        return name;
-    }
-}
-
-class DB{
-    HashMap<String,User> userDB;
-
-    DB(HashMap<String,User> userDB){
-        this.userDB = userDB;
-    }
-
-    public void signUp(){
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("이름을 입력해주세요. : ");
-        String name = sc.nextLine();
-
-        String id;
-        System.out.print("ID를 입력해주세요. : ");
-        id = sc.nextLine();
-        while(this.userDB.containsKey(id)){
-            System.out.print("사용 중인 ID입니다. 다른 ");
-            System.out.print("ID를 입력해주세요. : ");
-            id = sc.nextLine();
-        }
 
 
-        System.out.print("비밀번호를 입력해주세요. : ");
-        String password = sc.nextLine();
-
-        User user = new User(id,password, name);
-
-        this.userDB.put(id,user);
-
-        System.out.print("회원가입이 완료되었습니다. \n");
-        System.out.printf("아이디: %s\n",this.userDB.get(id).getId());
-        System.out.printf("이름: %s\n",this.userDB.get(id).getName());
-    }
-
-    public User login(){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("ID를 입력해주세요. : ");
-        String id = sc.nextLine();;
-
-        System.out.print("비밀번호를 입력해주세요. : ");
-        String password = sc.nextLine();
-
-        if(this.userDB.containsKey(id)){
-            if(this.userDB.get(id).getPw().equals(password)){
-                System.out.println("로그인을 성공하셨습니다.");
-                return this.userDB.get(id);
-            }else{
-                System.out.println("비밀번호가 틀렸습니다.");
-                return null;
-            }
-        }else{
-            System.out.println("존재하지 않은 ID입니다.");
-                return null;
-        }
-    }
-
-
-    public User updateInfo(String id) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("변경할 이름을 입력해주세요. : ");
-        String name = sc.nextLine();
-
-        System.out.print("변경할 비밀번호를 입력해주세요. : ");
-        String pw = sc.nextLine();
-
-        userDB.get(id).setUser(name, pw);
-        System.out.println("회원 정보 수정 완료!>.<");
-        return userDB.get(id);
-    }
-
-    public User deleteUser(String id) {
-        System.out.println("회원 탈퇴 완료! ㅜㅜ");
-        return userDB.remove(id);
-    }
-
-
-    public void getInfo(String id){
-        User user = this.userDB.get(id);
-        System.out.printf("ID : %s\n", user.getId());
-        System.out.printf("이름 : %s\n", user.getName());
-    }
-}
